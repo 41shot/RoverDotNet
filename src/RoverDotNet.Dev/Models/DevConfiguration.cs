81ae97998c2b64d5fc155fbde4e94ddd31c32a20
41shot <sc@41shot.com>
2026-07-08 15:19:56 +1200

    Removed unused Dev parameter.

--- src/RoverDotNet.Dev/Models/DevConfiguration.cs
+++ src/RoverDotNet.Dev/Models/DevConfiguration.cs
@@ -5,9 +5,6 @@ namespace RoverDotNet.Dev.Models;
 /// </summary>
 /// <param name="Subgraphs">The list of subgraphs to compose and watch.</param>
 /// <param name="RouterPort">The port on which the Apollo Router will listen (default: 4000).</param>
-/// <param name="SupergraphPort">
-/// Optional port for the supergraph endpoint (if different from <paramref name="RouterPort"/>).
-/// </param>
 /// <param name="RouterBinaryPath">
 /// Optional path to the Apollo Router binary. If null, the system will attempt to download
 /// or locate the router automatically.
@@ -23,7 +20,6 @@ namespace RoverDotNet.Dev.Models;
 public sealed record DevConfiguration(
     IReadOnlyList<SubgraphDefinition> Subgraphs,
     int RouterPort = 4000,
-    int? SupergraphPort = null,
     string? RouterBinaryPath = null,
     string? SupergraphConfigPath = null,
     string? RouterConfigPath = null,
